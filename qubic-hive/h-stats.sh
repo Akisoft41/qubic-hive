2c50a941b36e46338cabc4af106803e93a2c7ac3
Pascal Akermann <p.akermann@akisoft.ch>
2024-01-10 23:26:27 +0100

    v1.8.4

--- qubic-hive/h-stats.sh
+++ qubic-hive/h-stats.sh
@@ -31,7 +31,7 @@ log_basename="/var/log/miner/custom/custom"
 log_name="$log_basename.log"
 log_head_name="${log_basename}_head.log"
 conf_name="/hive/miners/custom/qubic-hive/appsettings.json"
-custom_version=1.8.3
+custom_version=1.8.4
 
 
 diffTime=$(get_log_time_diff)
